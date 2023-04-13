4e22a62b6fa865d41f7ab5e0b1570b623cb776ff
Keshav Agarwal <99586399+agkeshav@users.noreply.github.com>
2023-04-14 02:16:10 +0530

    Two Sum

---
+++ Arrays/Medium/Two_Sum.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> twoSum(vector<int> &nums, int target)
+{
+    unordered_map<int, int> mp;
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (mp.find(target - nums[i]) == mp.end())
+            mp[nums[i]] = i;
+        else
+            return {mp[target - nums[i]], i};
+    }
+
+    return {-1, -1};
+}
+int main()
+{
+
+    return 0;
+}
